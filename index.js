5b2f5b1722d9de1e39d585204f9322d373ef5e18
Manishkumar1114 <162983418+Manishkumar1114@users.noreply.github.com>
2024-08-14 22:04:56 +0530

    Added some extra feauters to cart and to edit the product

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 const path = require('path');
-const express = require("express");
-const bodyParser = require("body-parser");
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
 const app = express();
 
-const adminRouters = require('./routes/admin');
-const shopRouters = require('./routes/shop');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRouters);
-app.use(shopRouters);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.get('/contactus', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'views', 'contact.html'));
-});
+app.use(errorController.get404);
 
-app.post('/success', (req, res, next) => {
-  console.log('Name:', req.body.name);
-  console.log('Email:', req.body.email);
-  res.sendFile(path.join(__dirname, 'views', 'success.html'));
-});
-
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,18 @@
 const path = require('path');
+
 const express = require('express');
+
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-
-router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname , '../', 'views' , 'add-product.html')); 
-});
-
-
-router.post('/product', (req, res, next) => {
-  console.log(req.body); 
-  res.redirect('/');
-});
-
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
