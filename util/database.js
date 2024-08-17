ced63388f40c53959302c98656a7f31eae6af9be
Manishkumar1114 <162983418+Manishkumar1114@users.noreply.github.com>
2024-08-17 23:40:43 +0530

    learned about different sql command and how to implement them

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
-  const pool = mysql.createPool({
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 'Manish@1234'
-  });
+    password: 'Manish@123'
+});
 
-  module.exports = pool.promise();
+module.exports = pool.promise();
